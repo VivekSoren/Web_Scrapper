454fc5adb9bd337f4f0b6abe56f491c2a1f15cbf
Vivek6472 <viveknsoren@gmail.com>
2020-04-09 13:01:22 +0530

    Initial Commit

--- my_app/views.py
+++ my_app/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, template_name='my_app/base.html')
+    return render(request, template_name='base.html')
+
+
+def new_search(request):
+    return render(request, template_name='my_app/new_search.html')
