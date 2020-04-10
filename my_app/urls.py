045500e95c640da86e538c499f0b5d51191c27cd
Vivek6472 <viveknsoren@gmail.com>
2020-04-10 11:22:34 +0530

    Commit #2

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('new_search', views.new_search, name='new_search'),
