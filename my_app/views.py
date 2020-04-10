c81e61883983639956b551d594c34061dedeccff
Vivek6472 <viveknsoren@gmail.com>
2020-04-10 21:42:29 +0530

    commit #3

--- my_app/views.py
+++ my_app/views.py
@@ -1,11 +1,11 @@
 import requests
-import re
 from django.shortcuts import render
 from requests.compat import quote_plus
 from bs4 import BeautifulSoup
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://kolkata.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -34,28 +34,15 @@ def new_search(request):
         else:
             post_price = 'N/A'
 
-        '''
-        if post.find(class_='result-price'):
-            post_price = post.find(class_='result-price').text
-        else:
-            new_response = requests.get(post_url)
-            new_data = new_response.text
-            new_soup = BeautifulSoup(new_data, features='html.parser')
-            post_text = new_soup.find(id='postingbody').text
-
-            r1 = re.findall(r'\$\w+', post_text)
-            if r1:
-                post_price = r1[0]
-            else:
-                post_price = 'N/A'
-
         if post.find(class_='result-image').get('data-ids'):
-            post_image_id = post.find(class_='result-image').get('data-ids').split(',')
-            post_image_url = "https://images.craigslist.org/{}_300x300.jpg".format(post_image_id)
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
         else:
             post_image_url = 'https://craigslist.org/images/peace.jpg'
-        '''
-        final_postings.append((post_title, post_url, post_price))
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
     stuff_for_frontend = {
         'search': search,
         'final_postings': final_postings,
